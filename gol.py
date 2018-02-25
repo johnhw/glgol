293ce87f2688c969b98589ea845e880de5d27045
John Williamson <johnhw@gmail.com>
2018-02-25 14:45:10 +0000

    Implemented callahan lookup table

--- gol.py
+++ gol.py
@@ -5,6 +5,7 @@ from pyglet.gl import *
 import json
 # sphere stuff
 import pyspheregl.utils.glskeleton as skeleton
+from pyspheregl.utils.gloffscreen import FBOContext
 import pyspheregl.utils.np_vbo as np_vbo
 import pyspheregl.utils.shader as shader
 from pyspheregl.utils.shader import shader_from_file
@@ -20,65 +21,6 @@ wall_clock = timeit.default_timer
 import os
 import lifeparsers
 
-class Texture:
-    def __init__(self, target, id):
-        self.target = target
-        self.id = id
-
-class FBOContext:
-        
-    def __init__(self, width, height):
-        aspect = float(width)/float(height)
-
-        self.fbo_texture, self.fbo_buffer, self.fbo_renderbuffer = GLuint(), GLuint(), GLuint()
-        glGenTextures(1, self.fbo_texture)
-        glGenFramebuffers(1, self.fbo_buffer)
-        glBindFramebuffer(GL_FRAMEBUFFER, self.fbo_buffer) 
-        
-        self.texture = Texture(GL_TEXTURE_2D, self.fbo_texture)
-        # bind the texture and set its parameters
-        # create a texture
-        glBindTexture(GL_TEXTURE_2D, self.fbo_texture)
-        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE)
-        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT)
-        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT )
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST) 
-        glTexImage2D(GL_TEXTURE_2D, 0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE, None)
-        
-        # bind the frame buffer to the texture as the color render target        
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, self.fbo_texture, 0)
-                            
-
-        # create a depth buffer (as a render buffer) and attach it        
-        glGenRenderbuffers(1, self.fbo_renderbuffer)
-        glBindRenderbuffer(GL_RENDERBUFFER, self.fbo_renderbuffer)
-        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height)                
-        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, self.fbo_buffer)
-
-        # unbind the framebuffer/renderbuffer
-        glBindFramebuffer(GL_FRAMEBUFFER, 0)
-        glBindRenderbuffer(GL_RENDERBUFFER, 0)
-        
-        self.width = width
-        self.height = height
-        
-    
-
-    def __enter__(self):        
-        #enable render buffer
-        glBindFramebuffer(GL_FRAMEBUFFER, self.fbo_buffer)        
-        self.real_viewport = (GLint * 4)()
-        glGetIntegerv(GL_VIEWPORT, self.real_viewport)
-        glViewport(0, 0, self.width, self.height)
-        
-        
-    def __exit__(self, exc_type, exc_value, traceback):
-        # disable render buffer        
-        glBindFramebuffer(GL_FRAMEBUFFER, 0)
-        glViewport(*self.real_viewport)
-        
-      
 
 def mkshader(verts, frags, geoms=None):
     geoms = geoms or []
